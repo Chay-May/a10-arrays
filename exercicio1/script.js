0a53a307245691aebca3d0ec9bc1e3cd06c6faba
Chay-May <chayenne.mayara@gmail.com>
2022-07-27 23:01:17 -0300

    Exercícios off aula 010

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+const array1 = [56,8,7,3,95]
+console.log("Array números", array1)
+
+const array2 = ["banana", "maçã", "limão"]
+console.log("Array string", array2)
+
+const array3 = ["Data de nascimento", 1989, true]
+console.log("Array Mista", array3)
